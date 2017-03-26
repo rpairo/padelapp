b860f37903f901a973aabf3553f94b67ee774aab
Raúl Pera Pairó <raul_pairo@icloud.com>
2017-03-26 22:32:17 +0200

    Main toolbar
    Added main toolbar with icons: search and notifications. The ButterKnife library for view management has also been implemented.

--- app/src/main/java/com/nitsnets/padelapp/MainActivity.java
+++ app/src/main/java/com/nitsnets/padelapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.nitsnets.padelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar_main)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        configureToolbar();
+    }
+
+    private void configureToolbar() {
+        if (toolbar != null) setSupportActionBar(toolbar);
     }
 }
